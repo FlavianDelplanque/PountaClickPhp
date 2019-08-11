3a488b321cafd49ca299093ac1fa69656d4c52ab
MigTM <fatalflav2017@gmail.com>
2019-08-11 04:26:50 +0200

    ajout tableau score

--- scores.php
+++ scores.php
@@ -1,4 +1,6 @@
-<?php include "headerfooter/header.php"; ?>
+<?php include "headerfooter/header.php"; 
+require 'dbconnect.php'; 
+$reponse = $dbh->query('SELECT * FROM users'); ?>
 	<div class="global">
 		<table>
 			<thead>
@@ -9,11 +11,14 @@
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-				</tr>
+				<?php while($donnees = $reponse->fetch()){
+				echo "<tr><td>".$donnees['login']."</td><td>".$donnees['score']."</td><td>".$donnees['partiejouer']."</td></tr>";
+				} ?>
 			</tbody>
 		</table>
 		<a href="index.php"><button class="buttonAccueil">Accueil</button></a>
 		<a href="entrerpseudo.php"><button>Menu jeux</button></a>
 	</div>
 <?php include "headerfooter/footer.php"; ?>
+
+
